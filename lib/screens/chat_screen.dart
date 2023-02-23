a55dbbbdc97284761ee9d9a676d408f890f5396c
Alex Chinedu <nwekealexchinedu@gmail.com>
2023-02-23 01:20:29 +0100

    working on the chat screen ui

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:chatgpt_flutter/services/assets_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -11,11 +12,49 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final bool _isTyping = true;
+
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.asset(AssetsManager.openaiLogo),
+      appBar: AppBar(
+        elevation: 2,
+        leading: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: Image.asset(AssetsManager.openaiLogo),
+        ),
+        title: const Text('ChatGPT'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                itemCount: 6,
+                itemBuilder: ((context, index) {
+                  return const Text('Hello this a text.');
+                }),
+              ),
+            ),
+            if (_isTyping) ...[
+              const SpinKitThreeBounce(color: Colors.white, size: 18)
+            ]
+          ],
+        ),
       ),
     );
   }
