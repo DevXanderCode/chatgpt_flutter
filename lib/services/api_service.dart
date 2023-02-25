58afa40d5af951159ce18663c7daf29a9dea6c3c
Alex Chinedu <nwekealexchinedu@gmail.com>
2023-02-25 18:25:32 +0100

    add a little error handling to the get models api service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
@@ -7,10 +8,17 @@ import 'package:chatgpt_flutter/constants/api_consts.dart';
 class ApiService {
   static Future<void> getModels() async {
     try {
-      var response = await http.get(Uri.parse('$BASE_URL/models'),
+      var response = await http.get(Uri.parse('$BASE_URL/modelsaa'),
           headers: {"Authorization": "Bearer $API_KEY"});
 
       Map jsonResponse = jsonDecode(response.body);
+
+      if (jsonResponse['error'] != null) {
+        if (kDebugMode) {
+          print('response error ==> ${jsonResponse['error']['message']}');
+        }
+        throw HttpException(jsonResponse['error']['message']);
+      }
       if (kDebugMode) {
         print('reponse body $jsonResponse');
       }
