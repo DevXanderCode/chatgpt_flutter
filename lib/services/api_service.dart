94c180fa9991e3b0ec986bee9df0c06cf29c70eb
Alex Chinedu <nwekealexchinedu@gmail.com>
2023-02-25 23:00:02 +0100

    worked on the get models api service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,31 +1,38 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
+import 'package:chatgpt_flutter/models/models_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:chatgpt_flutter/constants/api_consts.dart';
 
 class ApiService {
-  static Future<void> getModels() async {
+  static Future<List<ModelsModel>> getModels() async {
+    log('function called');
     try {
-      var response = await http.get(Uri.parse('$BASE_URL/modelsaa'),
+      var response = await http.get(Uri.parse('$BASE_URL/models'),
           headers: {"Authorization": "Bearer $API_KEY"});
 
       Map jsonResponse = jsonDecode(response.body);
 
       if (jsonResponse['error'] != null) {
-        if (kDebugMode) {
-          print('response error ==> ${jsonResponse['error']['message']}');
-        }
+        log('response error ==> ${jsonResponse['error']['message']}');
+
         throw HttpException(jsonResponse['error']['message']);
       }
-      if (kDebugMode) {
-        print('reponse body $jsonResponse');
+
+      // print('reponse body $jsonResponse');
+      List temp = [];
+
+      for (var value in jsonResponse['data']) {
+        temp.add(value);
+        // print('temp ===>> ${value['id']}');
       }
+
+      return ModelsModel.modelsFromSnapshot(temp);
     } catch (error) {
-      if (kDebugMode) {
-        print('get models error ==> $error');
-      }
+      log('get models error ==> $error');
+      rethrow;
     }
   }
 }
--- lib/widgets/drop_down.dart
+++ lib/widgets/drop_down.dart
@@ -1,4 +1,6 @@
 import 'package:chatgpt_flutter/constants/constants.dart';
+import 'package:chatgpt_flutter/services/api_service.dart';
+import 'package:chatgpt_flutter/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class ModalDropDownWidget extends StatefulWidget {
@@ -12,7 +14,33 @@ class _ModalDropDownWidgetState extends State<ModalDropDownWidget> {
   String currentModel = 'Model1';
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
+    return FutureBuilder(
+        future: ApiService.getModels(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+                child: TextWidget(
+              label: snapshot.error.toString(),
+            ));
+          }
+          return snapshot.data == null || snapshot.data!.isEmpty
+              ? const SizedBox.shrink()
+              : DropdownButton(
+                  dropdownColor: scaffoldBackgroundColor,
+                  iconEnabledColor: Colors.white,
+                  items: getModelsItem,
+                  value: currentModel,
+                  onChanged: (value) {
+                    setState(() {
+                      currentModel = value.toString();
+                    });
+                  });
+        });
+  }
+}
+
+/* 
+  DropdownButton(
         dropdownColor: scaffoldBackgroundColor,
         iconEnabledColor: Colors.white,
         items: getModelsItem,
@@ -22,5 +50,5 @@ class _ModalDropDownWidgetState extends State<ModalDropDownWidget> {
             currentModel = value.toString();
           });
         });
-  }
-}
+
+*/
