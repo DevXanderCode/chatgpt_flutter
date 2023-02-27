7d737ff3028e31cb9ec5dc9b8ef9ed06e32d5c70
Alex Chinedu <nwekealexchinedu@gmail.com>
2023-02-28 00:15:57 +0100

    worked on sending message and displaying the response

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chatgpt_flutter/models/chat_model.dart';
+import 'package:chatgpt_flutter/providers/models_provider.dart';
 import 'package:chatgpt_flutter/services/api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:chatgpt_flutter/services/assets_manager.dart';
@@ -6,6 +8,7 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:chatgpt_flutter/constants/constants.dart';
 import "package:chatgpt_flutter/services/services.dart";
 import 'package:chatgpt_flutter/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -15,7 +18,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final bool _isTyping = true;
+  bool _isTyping = false;
 
   late TextEditingController textEditingController;
 
@@ -31,8 +34,11 @@ class _ChatScreenState extends State<ChatScreen> {
     super.dispose();
   }
 
+  List<ChatModel> chatList = [];
+
   @override
   Widget build(BuildContext context) {
+    final modelsProvider = Provider.of<ModelsProvider>(context);
     return Scaffold(
       appBar: AppBar(
           elevation: 2,
@@ -46,7 +52,10 @@ class _ChatScreenState extends State<ChatScreen> {
               onPressed: () async {
                 await Services.showModalSheet(context: context);
               },
-              icon: const Icon(Icons.more_vert_rounded, color: Colors.white),
+              icon: const Icon(
+                Icons.more_vert_rounded,
+                color: Colors.white,
+              ),
             )
           ]),
       body: SafeArea(
@@ -54,57 +63,79 @@ class _ChatScreenState extends State<ChatScreen> {
           children: [
             Flexible(
               child: ListView.builder(
-                itemCount: 6,
+                itemCount: chatList.length,
                 itemBuilder: ((context, index) {
                   return ChatWidget(
-                    msg: chatMessages[index]['msg'].toString(),
-                    chatIndex: int.parse(
-                      chatMessages[index]['chatIndex'].toString(),
-                    ),
+                    msg: chatList[index].msg,
+                    chatIndex: chatList[index].chatIndex,
                   );
                 }),
               ),
             ),
             if (_isTyping) ...[
-              const SpinKitThreeBounce(color: Colors.white, size: 18),
-              const SizedBox(height: 15),
-              Material(
-                color: cardColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          style: const TextStyle(color: Colors.white),
-                          controller: textEditingController,
-                          onSubmitted: (value) {
-                            // TODO: send message
-                          },
-                          decoration: const InputDecoration.collapsed(
-                            hintText: 'How can I help you',
-                            hintStyle: TextStyle(color: Colors.grey),
-                          ),
+              const SpinKitThreeBounce(color: Colors.white, size: 18)
+            ],
+            const SizedBox(height: 15),
+            Material(
+              color: cardColor,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        style: const TextStyle(color: Colors.white),
+                        controller: textEditingController,
+                        onSubmitted: (value) async {
+                          await sendMessageFCT(modelsProvider: modelsProvider);
+                        },
+                        decoration: const InputDecoration.collapsed(
+                          hintText: 'How can I help you',
+                          hintStyle: TextStyle(color: Colors.grey),
                         ),
                       ),
-                      IconButton(
-                        onPressed: () async {
-                          try {
-                            await ApiService.getModels();
-                          } catch (error) {
-                            print('Get Models Error ==> $error');
-                          }
-                        },
-                        icon: const Icon(Icons.send, color: Colors.white),
-                      )
-                    ],
-                  ),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        await sendMessageFCT(modelsProvider: modelsProvider);
+                      },
+                      icon: const Icon(Icons.send, color: Colors.white),
+                    )
+                  ],
                 ),
-              )
-            ]
+              ),
+            )
           ],
         ),
       ),
     );
+  }
+
+  Future<void> sendMessageFCT({required ModelsProvider modelsProvider}) async {
+    try {
+      setState(() {
+        _isTyping = true;
+        chatList.add(
+          ChatModel(
+            msg: textEditingController.text,
+            chatIndex: 0,
+          ),
+        );
+      });
+      // await ApiService.getModels();
+      chatList.addAll(
+        await ApiService.sendMessage(
+          message: textEditingController.text,
+          modelId: modelsProvider.getCurrentModel,
+        ),
+      );
+      setState(() {});
+    } catch (error) {
+      print('Get Models Error ==> $error');
+    } finally {
+      setState(() {
+        _isTyping = false;
+      });
+    }
   }
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
+import 'package:chatgpt_flutter/models/chat_model.dart';
 import 'package:chatgpt_flutter/models/models_model.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:chatgpt_flutter/constants/api_consts.dart';
 
@@ -35,16 +37,58 @@ class ApiService {
     }
   }
 
-  static Future<dynamic> sendMessage(
-      {required String message, required String model}) async {
+  static Future<List<ChatModel>> sendMessage(
+      {required String message, required String modelId}) async {
+    print('Send Message called model: $modelId, ====> message: $message');
     try {
-      var response = await http.post(Uri.parse('$BASE_URL/completions'), body: {
-        'models': model,
-        'prompt': message,
-        "max_tokens": 100,
-      });
+      var response = await http.post(
+        Uri.parse('$BASE_URL/completions'),
+        headers: {
+          "Authorization": "Bearer $API_KEY",
+          'Content-Type': 'application/json'
+        },
+        body: jsonEncode({
+          'model': modelId,
+          'prompt': message,
+          'max_tokens': 100,
+        }),
+      );
+
+      Map jsonResponse = jsonDecode(response.body);
+
+      if (jsonResponse['error'] != null) {
+        if (kDebugMode) {
+          print('response error ==> ${jsonResponse['error']['message']}');
+        }
+
+        throw HttpException(jsonResponse['error']['message']);
+      }
+      List<ChatModel> chatList = [];
+      if (jsonResponse['choices'].length > 0) {
+        // if (kDebugMode) {
+        //   print(
+        //     'jsonResponse[choice]text ===> ${jsonResponse['choices'][0]['text']}',
+        //   );
+        // }
+
+        chatList = List.generate(
+          jsonResponse['choices'].length,
+          (index) => ChatModel(
+            msg: jsonResponse['choices'][index]['text'],
+            chatIndex: 1,
+          ),
+        );
+      }
+
+      if (kDebugMode) {
+        print('Message reponse $chatList');
+      }
+
+      return chatList;
     } catch (e) {
-      log('send message error ==> $e');
+      if (kDebugMode) {
+        print('send message error ==> $e');
+      }
       rethrow;
     }
   }
